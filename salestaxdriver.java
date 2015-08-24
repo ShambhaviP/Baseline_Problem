4ed7c1699a9bd2517d77616f8135d05ec5ac3ba2
Shambhavi <shambhap@INshambhap.local>
2015-08-24 18:03:15 +0530

    fourth commit

--- salestaxdriver.java
+++ salestaxdriver.java
@@ -6,7 +6,22 @@ import java.util.ArrayList;
  * Created by shambhap on 24/08/15.
  */
 
+class Item{
 
+    int qty;
+    String name;
+    double price;
+
+    Item(int q,String n,double p)
+    {
+        qty=q;
+        name=n;
+        price=p;
+
+    }
+
+
+}
 
 
 
@@ -15,12 +30,14 @@ public class salestaxdriver {
     public static  void main(String args[]) throws IOException
     {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        /*BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Give Input");
-        String input = br.readLine();
-
+        String input = br.readLine();*/
+       Item obj1=new Item(1,"Book",12.49);
+        Item obj2=new Item(1,"Music CD",14.99);
+        Item obj3=new Item(1,"chocolate bar",0.85);
         ArrayList<String> items=new ArrayList<String>();
-        
+
 
         salestaxcalculator();
     }
