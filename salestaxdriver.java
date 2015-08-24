b9816c8ed24ebd93e2d4ff630c52320538978c7d
Shambhavi <shambhap@INshambhap.local>
2015-08-24 18:21:42 +0530

    fifth commit

--- salestaxdriver.java
+++ salestaxdriver.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.ArrayList;
+import  java.util.Iterator;
 
 
 /**
@@ -36,10 +37,34 @@ public class salestaxdriver {
        Item obj1=new Item(1,"Book",12.49);
         Item obj2=new Item(1,"Music CD",14.99);
         Item obj3=new Item(1,"chocolate bar",0.85);
-        ArrayList<String> items=new ArrayList<String>();
+        ArrayList<Item> items=new ArrayList<Item>();
+        items.add(obj1);
+        items.add(obj2);
+        items.add(obj3);
 
 
+       total(items);
         salestaxcalculator();
+    }
+
+    private static void total(ArrayList<Item> items) {
+   double sum=0;
+
+      /*Iterator itr=items.iterator();
+        while(itr.hasNext()){
+
+            double cost=itr.next().
+
+
+
+        }*/
+
+        for(Item item1:items){
+      double cost=item1.price;
+      sum+=cost;
+        }
+        
+
     }
 
     private static void salestaxcalculator() {
