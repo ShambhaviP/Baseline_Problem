d376a6f388e086994e00d5e140ecabb73f036056
Shambhavi <shambhap@INshambhap.local>
2015-08-24 17:52:08 +0530

    third commit

--- salestaxdriver.java
+++ salestaxdriver.java
@@ -1,9 +1,13 @@
 import java.io.*;
+import java.util.ArrayList;
 
 
 /**
  * Created by shambhap on 24/08/15.
  */
+
+
+
 
 
 public class salestaxdriver {
@@ -14,5 +18,17 @@ public class salestaxdriver {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Give Input");
         String input = br.readLine();
+
+        ArrayList<String> items=new ArrayList<String>();
+        
+
+        salestaxcalculator();
     }
+
+    private static void salestaxcalculator() {
+
+
+    }
+
+
 }
