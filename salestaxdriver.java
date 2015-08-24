c08c169180046550b4bf5bf587a6d36910865a1b
Shambhavi <shambhap@INshambhap.local>
2015-08-24 17:42:02 +0530

    second commit

--- salestaxdriver.java
+++ salestaxdriver.java
@@ -1,7 +1,18 @@
+import java.io.*;
+
+
 /**
  * Created by shambhap on 24/08/15.
  */
-public class salestaxdriver {
-    
 
+
+public class salestaxdriver {
+
+    public static  void main(String args[]) throws IOException
+    {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Give Input");
+        String input = br.readLine();
+    }
 }
